71c5350f24ce6890a07cd8ff1a7a6685968d689e
Vinton Lee <vinton.e.lee@gmail.com>
2019-08-15 16:08:31 -0600

    finnished jthw ex 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+	public static void main( String[] args ) {
+		System.out.println( "I, Vinton Lee, am determined to learn how to code." );
+		System.out.println( "Today's date is Aug 14, 2019" );
+	}
+}
