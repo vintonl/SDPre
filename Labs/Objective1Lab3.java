b294ca2415bec28fce466b6f4dfecd777078f38b
Vinton Lee <vinton.e.lee@gmail.com>
2019-08-16 11:12:16 -0600

    completed Objective1Lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,13 @@
+// output: This program should print out your name, age, and the state in which you were born in the following format.
+// Name : Peter Williams
+// Age : 32
+// State : New York
+
+public class Objective1Lab3 {
+
+  public static void main (String[] args) {
+    System.out.println("Name : Vinton Lee");
+    System.out.println("Age : 37");
+    System.out.println("State : District of Columbia");
+  }
+}
