907e51e9047fd03f0eed84546f93db06acdaf9c6
Vinton Lee <vinton.e.lee@gmail.com>
2019-08-15 20:42:28 -0600

    finished jthw ex. 2

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,18 @@
+public class GasolineReceipt
+{
+	public static void main( String[] args )
+	{
+		System.out.println( "+------------------------+" );
+		System.out.println( "|                        |" );
+		System.out.println( "|      CORNER STORE      |" );
+		System.out.println( "|                        |" );
+		System.out.println( "|  2019-04-19  4:38PM    |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| Gallons:       10.870  |" );
+		System.out.println( "| Price/gallon: $ 2.890  |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| Fuel total:  $ 30.71   |" );
+		System.out.println( "|                        |" );
+		System.out.println( "+------------------------+" );
+	}
+}
