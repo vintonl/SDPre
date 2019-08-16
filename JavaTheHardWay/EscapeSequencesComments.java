58502677eeb5a45e3029608ffd33df5fda94fddf
Vinton Lee <vinton.e.lee@gmail.com>
2019-08-15 21:12:45 -0600

    finished jthw ex 3

---
+++ JavaTheHardWay/EscapeSequencesComments.java
@@ -0,0 +1,17 @@
+public class EscapeSequencesComments {
+	public static void main( String [] args ) {
+		// This exercise demonstrates escape sequences & comments (Like these)!
+		System.out.print( "Learn\tJava\n\tthe\nHard\tWay\n\n" );
+		System.out.print( "\tLearn Java the \"Hard\" Way!\n" );
+		// System.out.frimp( " Learn Java the Hard Way" );
+		System.out.print( "Hello\n" ); // This line prints Hello.
+		System.out.print( "Jello\by\n" ); //This line prints Jelly.
+		/* The quick brown fox jumped over a laxy dog.
+			Quickly wafting zephyrs vex bold Jim. */
+		System.out /* testing */ .println( "Hard to believe, eh?" );
+		System.out.println( "Surprised? /* abcde */ Or what did you expect?" );
+		System.out.println( "\\ // -=-\\ //" );
+		System.out.println( "\\\\ \\\\\\ \\\\\\\\" ); // it takes 2 to make 1
+		System.out.print( " I hope you understand \"escape sequences\" now.\n" );
+	}
+}		
