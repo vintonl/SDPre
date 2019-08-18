2177389bfd991b62418b01f4da5cd0e3ceda2e5d
Vinton Lee <vinton.e.lee@gmail.com>
2019-08-18 11:49:46 -0600

    completed Objective2Lab4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,21 @@
+// Lab 4 - Writing Expressions
+// Overview
+// Create a file called Objective2Lab4.java in the SDPre/Labs directory.
+// Using the Starter Code below add code to the commented area that would:
+// Assign values to num1, num2, and num3.
+// Calculate the average of those numbers and store the resulting value into average.
+// Print out the calculate average.
+
+public class Objective2Lab4 {
+  public static void main (String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 15;
+    num2 = 6;
+    num3 = 2;
+
+    average = (num1 + num2 + num3)/3;
+
+    System.out.println(average);
+  }
+}
