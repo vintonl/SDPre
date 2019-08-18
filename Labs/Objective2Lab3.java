dba5eb557274b36426ef385153aa5d7140ddc598
Vinton Lee <vinton.e.lee@gmail.com>
2019-08-18 11:39:06 -0600

    completed Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,22 @@
+// Lab 3 - Creating and Using Variables
+// Overview
+// Create a file called Objective2Lab3.java in the SDPre/Labs directory.
+// Using the Starter Code below add code to the commented area that would:
+// Assign values of your 3 favorite foods to the provided variables.
+// Print the String 'My Favorite Foods Are:'
+// Once the values are assigned print them out on there own lines.
+
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String food1, food2, food3;
+
+    food1 = "BBQ";
+    food2 = "Chicken Piccata";
+    food3 = "Fajitas";
+
+    System.out.println("My Favorite Foods Are:");
+    System.out.println("1. " + food1);
+    System.out.println("2. " + food2);
+    System.out.println("3. " + food3);
+  }
+}
