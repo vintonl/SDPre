1bac492177412c21841793a6189e507b23db355a
Vinton Lee <vinton.e.lee@gmail.com>
2019-08-18 11:18:34 -0600

    completed Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,23 @@
+// Lab 2 - Printing Multiple Variables
+// Overview
+// Create a file called Objective2Lab2.java in the SDPre/Labs directory.
+// Using the Starter Code below add code to the commented area that would:
+// print out your name, age and the state where you were born.
+// Change the values to reflect your own personal information.
+
+public class Objective2Lab2 {
+  public static void main (String[] args) {
+    String name = "Charles Peterson";
+    int age = 47;
+    String state = "Arkansas";
+
+    name =  "Vinton";
+    age = 37;
+    state = "D.C.";
+
+    System.out.println("My name is " + name + ", I am " + age + ", and I was born in " + state);
+
+
+
+  }
+}
