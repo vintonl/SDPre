f84b43a991b61ce34bd8b592542dc73ea68af254
Vinton Lee <vinton.e.lee@gmail.com>
2019-08-18 11:06:24 -0600

    completed Objective 2 Lab 1

---
+++ Labs/Objective2Lab1.java
@@ -0,0 +1,13 @@
+// Lab 1 - Printing a Variable
+// Overview
+// Create a file called Objective2Lab1.java in the SDPre/Labs directory.
+// Using the Starter Code below add code to the commented area that would print out the value "Hello World" stored in the hello variable.
+
+
+public class Objective2Lab1 {
+  public static void main (String[] args) {
+    String hello = "Hello World";
+
+    System.out.println(hello);
+  }
+}
