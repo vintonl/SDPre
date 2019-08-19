b8d415f2f7a346b15e95398b6551002eb605d5d9
Vinton Lee <vinton.e.lee@gmail.com>
2019-08-19 10:49:03 -0600

    completed Objective3Lab3

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,31 @@
+// Lab 3 - Calculate Birth Year
+// Overview
+// Create a file called Objective3Lab3.java in the SDPre/Labs directory.
+// Using the Starter Code below add code to the commented area that would:
+// Take in the users age using input.nextInt() and store it in the age variable.
+// Calculates what year the user was born in.
+// Prints out the calculated birthYear.
+
+// input: string - user input - age variable
+// output: calculate and println birthYear
+
+import java.util.Scanner;
+import java.util.Calendar;
+
+public class Objective3Lab3 {
+  public static void main (String[] args) {
+    int birthYear, age;
+
+    Calendar cal = Calendar.getInstance();
+    int currentYear = cal.get(Calendar.YEAR);
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("How old are you?");
+    age = input.nextInt();
+
+    birthYear = currentYear - age;
+
+    System.out.println("You were born in " + birthYear); // only works if user has already had birthday in calendar year
+    input.close();
+    }
+}
