a6083acc6784ef69e30417e2b641afd147a47acf
Vinton Lee <vinton.e.lee@gmail.com>
2019-08-20 20:26:05 -0600

    finshed jthw Ex 17

---
+++ JavaTheHardWay/ClubBouncer.java
@@ -0,0 +1,18 @@
+// Exercise 17: Otherwise (If Statments with else)
+
+public class ClubBouncer {
+  public static void main ( String[] args ) {
+      int age = 22;
+      boolean onGuestList = false;
+      double allure = 7.5;
+      String gender = "F";
+
+      if (onGuestList || age >= 21 || (gender.equals("F") && allure >= 8)) {
+          System.out.println("Hi.");
+          System.out.println("You are allowed to enter the club.");
+      }
+      else {
+          System.out.println("You are not allowed to enter the club.");
+      }
+  }
+}
