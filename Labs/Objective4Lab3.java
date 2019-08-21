652f523a6e9db5a739c8da126158d5b54a7342ff
Vinton Lee <vinton.e.lee@gmail.com>
2019-08-21 12:47:33 -0600

    completed Objective4Lab3

---
+++ Labs/Objective4Lab3.java
@@ -0,0 +1,26 @@
+// input: Scanner - user  - number
+// output: string - The number is positive, or The number is negative
+
+import java.util.Scanner;
+
+public class Objective4Lab3 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int userNum;
+
+    System.out.println("Please enter a number: ");
+    userNum = scanner.nextInt();
+
+    if (userNum > 0) {
+      System.out.println("The number is positive");
+    }
+    else if (userNum < 0){
+      System.out.println("The number is negative");
+    }
+    else {
+      System.out.println("The number is neither positive nor negative");
+    }
+
+    scanner.close();
+  }
+}
