93abe624d121909a951986c5eea303ff74d13f75
Vinton Lee <vinton.e.lee@gmail.com>
2019-08-21 12:21:55 -0600

    completed Objective4Lab1

---
+++ Labs/Objective4Lab1.java
@@ -0,0 +1,20 @@
+// Lab 1 - Find Max Value
+// Overview
+// Create a file called Objective4Lab1.java in the SDPre/Labs directory.
+// Using the Starter Code below add code to the commented areas that would:
+// Determine if num1 is greater than or less than num2 (if statements provided).
+// Print out the findings.
+
+public class Objective4Lab1 {
+  public static void main(String[] args) {
+    int num1 = 5;
+    int num2 = 15;
+
+    if (num1 > num2) {
+      System.out.println(num1 + " is greater than " + num2);
+    }
+    else {
+      System.out.println(num2 + " is greater than " + num1);
+    }
+  }
+}
