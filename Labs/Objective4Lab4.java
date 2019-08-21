36c9768842866a35c094b5d1cf175ea3c3e26083
Vinton Lee <vinton.e.lee@gmail.com>
2019-08-21 13:01:01 -0600

    completed Objective4Lab4

---
+++ Labs/Objective4Lab4.java
@@ -0,0 +1,23 @@
+// input: userNum
+// output: string - "The number is odd" or "The number is even"
+
+import java.util.Scanner;
+
+public class Objective4Lab4 {
+  public static void main(String[] args) {
+    int userNum;
+    Scanner keyboard = new Scanner(System.in);
+
+    System.out.println("Please enter a number (no decimal): "); //decimal will chrash program
+    userNum = keyboard.nextInt();
+
+    if (userNum % 2 == 0) {
+      System.out.println("The number is even");
+    }
+    else {
+      System.out.println("The number is odd");
+    }
+
+    keyboard.close();
+  }
+}
