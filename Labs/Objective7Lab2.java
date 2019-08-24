63f7513215b4e38d7821aa2c835d1678306750b1
Vinton Lee <vinton.e.lee@gmail.com>
2019-08-24 15:35:01 -0600

    completed Objective7Lab2

---
+++ Labs/Objective7Lab2.java
@@ -0,0 +1,17 @@
+// Lab 2 - Print Message
+// Overview
+// Create a file called Objective7Lab2.java in the SDPre/Labs directory.
+// Using the Starter Code below add code to the commented areas that would:
+// Write code in the provided printMessage method that would print out "In method".
+
+public class Objective7Lab2 {
+  public static void main(String[] args) {
+    System.out.println("In main");
+    printMessage();
+    System.out.println("In main");
+  }
+
+  public static void printMessage() {
+    System.out.println("In method");
+  }
+}
