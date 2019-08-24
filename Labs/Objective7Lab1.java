234b995147334288ae352f2907af1ed6b133baac
Vinton Lee <vinton.e.lee@gmail.com>
2019-08-24 15:29:50 -0600

    completed Objective7Lab1

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,15 @@
+// Lab 1 - Print via Method
+// Overview
+// Create a file called Objective7Lab1.java in the SDPre/Labs directory.
+// Using the Starter Code below add code to the commented areas that would:
+// Write code in the main to call the provided printName method.
+
+public class Objective7Lab1 {
+  public static void main(String[] args) {
+    printName();
+  }
+
+  public static void printName() {
+    System.out.println("Hello Doug");
+  }
+}
