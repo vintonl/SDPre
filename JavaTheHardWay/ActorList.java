f42f127b46ff5191192d7de3df69d05b72efcde6
Vinton Lee <vinton.e.lee@gmail.com>
2019-09-04 17:09:37 -0600

    finished jthw ex 54

---
+++ JavaTheHardWay/ActorList.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class Actor {
+	String name;
+	String role;
+	String birthdate;
+}
+
+public class ActorList {
+	public static void main(String[] args) throws Exception {
+		String url = "https://learnjavathehardway.org/txt/s01e01-cast.txt";
+		// Scanner inFile = new Scanner(new java.io.File("s01e01-cast.txt"));
+		Scanner inFile = new Scanner((new java.net.URL(url)).openStream());
+    int count = 0;
+
+		while ( inFile.hasNext() ) {
+			Actor a = getActor(inFile);
+			System.out.print(a.name + " was born on " + a.birthdate);
+			System.out.println(" and played " + a.role);
+			count++;
+		}
+		inFile.close();
+
+		System.out.println("\t" + count + " records were processed.");
+	}
+
+  public static Actor getActor( Scanner input ) {
+		Actor a = new Actor();
+		a.name = input.nextLine();
+		a.role = input.nextLine();
+		a.birthdate = input.nextLine();
+
+		return a;
+	}
+}
